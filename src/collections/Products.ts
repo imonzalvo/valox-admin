26b4c5ebd450e2705455c333ea83850fc38f9a7b
Ignacio Monzalvo <ignaciomonzalvo@192.168.1.4>
2023-01-12 21:14:05 -0300

    First commit

---
+++ src/collections/Products.ts
@@ -0,0 +1,64 @@
+import { CollectionConfig } from 'payload/types';
+
+const Products: CollectionConfig = {
+  slug: 'products',
+  admin: {
+    defaultColumns: ['title', 'image'],
+    useAsTitle: 'Productos',
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+    },
+    {
+      name: 'author',
+      type: 'relationship',
+      relationTo: 'users',
+    },
+    {
+      name: 'publishedDate',
+      type: 'date',
+    },
+    {
+      name: 'category',
+      type: 'relationship',
+      relationTo: 'categories'
+    },
+    {
+      name: 'tags',
+      type: 'relationship',
+      relationTo: 'tags',
+      hasMany: true,
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+    {
+      name: 'status',
+      type: 'select',
+      options: [
+        {
+          value: 'draft',
+          label: 'Draft',
+        },
+        {
+          value: 'published',
+          label: 'Published',
+        },
+      ],
+      defaultValue: 'draft',
+      admin: {
+        position: 'sidebar',
+      }
+    }
+  ],
+}
+
+export default Products;

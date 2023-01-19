637baaac8acbb482d7dff81deaa2ebf22d58188d
Ignacio Monzalvo <ignaciomonzalvo@Ignacios-MacBook-Pro.local>
2023-01-18 21:39:15 -0300

    rc0.0.1

--- src/colorPicker/config.ts
+++ src/colorPicker/config.ts
@@ -1,14 +1,18 @@
-import { Field } from 'payload/types';
-import Cell from './Cell';
-import InputField from './InputField';
+import { Field, Validate } from "payload/types";
+import Cell from "./Cell";
+import InputField from "./InputField";
 
-export const validateHexColor = (value: string): boolean | string => {
-  return value && value.match(/^#(?:[0-9a-fA-F]{3}){1,2}$/) !== null || `Please give a valid hex color`;
-}
+export const validateHexColor: Validate<any, any, any> = (
+  value: string
+): true | string => {
+  return value && value.match(/^#(?:[0-9a-fA-F]{3}){1,2}$/) !== null
+    ? `Please give a valid hex color`
+    : true;
+};
 
 const colorField: Field = {
-  name: 'color',
-  type: 'text',
+  name: "color",
+  type: "text",
   validate: validateHexColor,
   defaultValue: "#000",
   required: true,

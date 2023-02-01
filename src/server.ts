e7f687485dbce359d170d250569a02d9e6fa7394
Ignacio Monzalvo <ignaciomonzalvo@192.168.1.8>
2023-01-31 21:34:35 -0300

    void

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
-  res.setHeader("Access-Control-Allow-Origin", "https://master.d1rpsr8tnprxdk.amplifyapp.com/");
+  res.setHeader("Access-Control-Allow-Origin", "https://master.d1rpsr8tnprxdk.amplifyapp.com");
 
   // Request methods you wish to allow
   res.setHeader(

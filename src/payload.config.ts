cfc2d3c2e7539244ae8c23e4030baf5e5b8a60fd
Ignacio Monzalvo <ignaciomonzalvo@192.168.1.5>
2023-03-06 22:06:19 -0300

    fix

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,31 +1,31 @@
-import { buildConfig } from 'payload/config';
-import path from 'path';
-import { cloudStorage } from '@payloadcms/plugin-cloud-storage';
-import { s3Adapter } from '@payloadcms/plugin-cloud-storage/s3';
+import { buildConfig } from "payload/config";
+import path from "path";
+import { cloudStorage } from "@payloadcms/plugin-cloud-storage";
+import { s3Adapter } from "@payloadcms/plugin-cloud-storage/s3";
 
-import Categories from './collections/Categories';
-import Products from './collections/Products';
-import Tags from './collections/Tags';
-import Users from './collections/Users';
-import Media from './collections/Media';
-import { Sites } from './collections/Sites';
-import Companies from './collections/Company';
-import { seed } from './seed';
-import Configurations from './collections/Configuration';
-import Orders from './collections/Orders';
-import ShippingOptions from './collections/ShippingOptions';
-import PaymentMethods from './collections/PaymentMethods';
-import OrderProducts from './collections/OrderProducts';
+import Categories from "./collections/Categories";
+import Products from "./collections/Products";
+import Tags from "./collections/Tags";
+import Users from "./collections/Users";
+import Media from "./collections/Media";
+import { Sites } from "./collections/Sites";
+import Companies from "./collections/Company";
+import { seed } from "./seed";
+import Configurations from "./collections/Configuration";
+import Orders from "./collections/Orders";
+import ShippingOptions from "./collections/ShippingOptions";
+import PaymentMethods from "./collections/PaymentMethods";
+import OrderProducts from "./collections/OrderProducts";
 
 const adapter = s3Adapter({
   config: {
     credentials: {
       accessKeyId: process.env.S3_ACCESS_KEY_ID,
       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    }
+    },
   },
   bucket: process.env.S3_BUCKET,
-})
+});
 
 export default buildConfig({
   serverURL: process.env.HOST_NAME,
@@ -33,10 +33,10 @@ export default buildConfig({
   plugins: [
     cloudStorage({
       collections: {
-        'media': {
+        media: {
           adapter: adapter,
           disableLocalStorage: true,
-          prefix: "media/images"
+          prefix: "media/images",
         },
       },
     }),
@@ -56,18 +56,18 @@ export default buildConfig({
     Orders,
     ShippingOptions,
     PaymentMethods,
-    OrderProducts
+    OrderProducts,
   ],
   typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts')
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
   graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
   },
   onInit: async (payload) => {
     // If the `env` var `PAYLOAD_SEED` is set, seed the db
     if (process.env.PAYLOAD_SEED) {
       await seed(payload);
     }
-  }
+  },
 });

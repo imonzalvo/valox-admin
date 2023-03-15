3b2e0a8b26451ad74ce839a5658a65ef24e8bea5
Ignacio Monzalvo <ignaciomonzalvo@192.168.1.4>
2023-03-15 20:54:08 -0300

    Fix

--- src/collections/Media/hooks/renameMedia.ts
+++ src/collections/Media/hooks/renameMedia.ts
@@ -1,25 +1,33 @@
 import type { BeforeChangeHook } from "payload/dist/collections/config/types";
 
-const removeWhiteSpaces = (filename) => {
-  return filename.replace(/\s/g, "");
+const formatFileName = (filename: string) => {
+  const newFileName = filename.replace(/\s/g, "");
+
+  const random = (Math.random() + 1).toString(36).substring(7);
+
+  return `${random}-${newFileName}`;
 };
 export const renameMedia: BeforeChangeHook = ({ data, req, operation }) => {
   if (operation === "create") {
     let sizes = Object.keys(data["sizes"]);
 
     sizes.forEach((size) => {
-      console.log("size", data[size], size, data["sizes"]["tablet"]);
-      
+      console.log("size::", data, size);
+
       if (!!data["sizes"][size]["filename"]) {
-        data["sizes"][size]["filename"] = removeWhiteSpaces(
+        data["sizes"][size]["filename"] = formatFileName(
           data["sizes"][size]["filename"]
         );
       }
     });
 
+    console.log("data::", {
+      ...data,
+      filename: formatFileName(data.filename),
+    });
     return {
       ...data,
-      filename: removeWhiteSpaces(data.filename),
+      filename: formatFileName(data.filename),
     };
   }
 
